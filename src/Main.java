72c4b1570e9d886233bdb7a9bd084ed609ac1244
AlexandrP88 <alexsandr@rtural.ru>
2021-10-28 23:05:52 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        float ticketPrice = 10300.50F;
+        int milePrice = 20;
+        float amountMiles = (int) ticketPrice / milePrice;
+        System.out.println(amountMiles);
+    }
+}
